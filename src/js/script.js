7c4b0b9d3d3c2b27863efa5232d19af76ee11187
Svetlana <sveta-dekt@tut.by>
2021-03-03 17:22:55 +0300

    dist

--- src/js/script.js
+++ src/js/script.js
@@ -109,8 +109,8 @@ $(document).ready(function(){
     }
   });
 
-  $("a[href^='#']").click(function(){
-    var _href = $(this).attr("href");
+  $("a[href=#up]").click(function(){
+    const _href = $(this).attr("href");
     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
     return false;
 });
